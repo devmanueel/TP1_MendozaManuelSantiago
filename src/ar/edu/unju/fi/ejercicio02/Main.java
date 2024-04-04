9a71c7cb97fb77b2c763a2a3115d847577eb5184
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-04 20:04:54 -0300

    tercer commit MendozaManuel Prueba

---
+++ src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long telefono = 3884605498l;
+		Double resultaDouble = Math.cos(0.5);
+		
+	}
+
+}
