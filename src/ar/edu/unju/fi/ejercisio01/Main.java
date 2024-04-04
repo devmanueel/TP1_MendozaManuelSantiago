ee43a62807c98e82b4534b098607e6c713cd0b3e
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-04 15:27:35 -0300

    Segundo Commit Punto 1 MendozaManuelSantiago

---
+++ src/ar/edu/unju/fi/ejercisio01/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercisio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		int numero01 = 5;
+		int numero02 = 6;
+		double promedio = (double) (numero01 + numero02)/2;
+		System.out.println(promedio);
+		**/
+		
+		int primerNumero = 8;
+		int segundoNumero = 5;
+		int tercerNumero = 1;
+		int cuartoNumero = 20;
+		int quintoNumero = 9;
+		double promedio = (double) (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println("El promedio es: "+ promedio );
+		System.out.println("Numero1: "+ primerNumero+" --Numero2: "+ segundoNumero+" --Numero3: "+ tercerNumero+" --Numero4: "+ cuartoNumero+" --Numero5: "+ quintoNumero);
+		
+	}
+
+}
