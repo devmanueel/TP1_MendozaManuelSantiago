79039720da9ca332f4ff32b02400294bf85c067b
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-08 00:34:21 -0300

    tree commit

--- src/ar/edu/unju/fi/ejercisio01/Main.java
+++ src/ar/edu/unju/fi/ejercisio01/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/**
+		/**s
 		int numero01 = 5;
 		int numero02 = 6;
 		double promedio = (double) (numero01 + numero02)/2;
