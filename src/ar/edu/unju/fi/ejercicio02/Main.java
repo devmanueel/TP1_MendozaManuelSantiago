0476850105ce6b13f2ab8687017363c3ed4856ed
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-09 18:54:47 -0300

    Punto Numero 2 Mendoza Manuel Santiago

--- src/ar/edu/unju/fi/ejercicio02/Main.java
+++ src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -1,11 +1,21 @@
 package ar.edu.unju.fi.ejercicio02;
 
+import java.security.PublicKey;
+
+//punto 2
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String pais = "Argentina";
+		int edad = 23;
+		float altura = 1.72f;
+		float precioProducto = 899.99f; 
 		long telefono = 3884605498l;
-		Double resultaDouble = Math.cos(0.5);
+		double resultaDouble = Math.cos(0.5);
+		
+		System.out.println("Pais: "+pais+"\nEdad: "+edad+ "\nAltura: "+altura+"\nPrecio del Producto: "+precioProducto
+				+"\nTelefono: "+telefono+"\nCoseno de 0.5: "+resultaDouble);
 		
 	}
 
